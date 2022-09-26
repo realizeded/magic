6fea2a1ebfa6f98efc40961141e0c71a48f12548
lian <2235500875@qq.com>
2022-09-26 23:59:20 +0800

    feat: 自动化构建部署测试

--- packages/server/ormconfig.js
+++ packages/server/ormconfig.js
@@ -1,25 +1,22 @@
 const env = process.env.NODE_ENV;
-console.log(env);
-const entitiesPath =
-  env === "production"
-    ? "build/models/entity/**/*.js"
-    : "src/models/entity/**/*.ts";
+
+const entitiesPath = env === 'production' ? 'build/models/entity/**/*.js' : 'src/models/entity/**/*.ts';
 
 module.exports = {
-  type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "19981020",
-  database: "template",
-  synchronize: true,
-  logging: false,
-  entities: [entitiesPath],
-  migrations: ["src/models/migration/**/*.ts"],
-  subscribers: ["src/models/subscriber/**/*.ts"],
-  cli: {
-    entitiesDir: "src/models/entity",
-    migrationsDir: "src/models/migration",
-    subscribersDir: "src/models/subscriber",
-  },
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '19981020',
+    database: 'template',
+    synchronize: true,
+    logging: false,
+    entities: [entitiesPath],
+    migrations: ['src/models/migration/**/*.ts'],
+    subscribers: ['src/models/subscriber/**/*.ts'],
+    cli: {
+        entitiesDir: 'src/models/entity',
+        migrationsDir: 'src/models/migration',
+        subscribersDir: 'src/models/subscriber'
+    }
 };
