c25da0b022cf5dff4f9ae90852f0c2acde394bab
lian <2235500875@qq.com>
2022-09-26 23:52:03 +0800

    feat: 自动化构建部署测试

--- packages/client/config/webpack.config.js
+++ packages/client/config/webpack.config.js
@@ -324,7 +324,7 @@ module.exports = function (webpackEnv) {
                     babelRuntimeRegenerator
                 ]),
                 new DefinePlugin({
-                    isOnline: JSON.stringify(isEnvProduction)
+                    isOnline: process.env.NODE_ENV === 'production'
                 })
             ]
         },
