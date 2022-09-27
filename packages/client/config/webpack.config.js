725d06dcb67a178897b2b9516ee31d4c26424603
lian <2235500875@qq.com>
2022-09-27 21:26:04 +0800

    feat: sdk

--- packages/client/config/webpack.config.js
+++ packages/client/config/webpack.config.js
@@ -202,14 +202,12 @@ module.exports = function (webpackEnv) {
             pathinfo: isEnvDevelopment,
             // There will be one main bundle, and one file per asynchronous chunk.
             // In development, it does not produce real files.
-            filename: isEnvProduction
-                ? 'static/js/[name].[contenthash:8].js'
-                : isEnvDevelopment && 'static/js/bundle.js',
-            // There are also additional JS chunk files if you use code splitting.
-            chunkFilename: isEnvProduction
-                ? 'static/js/[name].[contenthash:8].chunk.js'
-                : isEnvDevelopment && 'static/js/[name].chunk.js',
-            assetModuleFilename: 'static/media/[name].[hash][ext]',
+            filename: isEnvProduction ? 'static/js/sdk.js' : isEnvDevelopment && 'static/js/bundle.js',
+            // // There are also additional JS chunk files if you use code splitting.
+            // chunkFilename: isEnvProduction
+            //     ? 'static/js/[name].[contenthash:8].chunk.js'
+            //     : isEnvDevelopment && 'static/js/[name].chunk.js',
+            // assetModuleFilename: 'static/media/[name].[hash][ext]',
             // webpack uses `publicPath` to determine where the app is being served from.
             // It requires a trailing slash, or the file assets will get an incorrect path.
             // We inferred the "public path" (such as / or /my-project) from homepage.
@@ -604,6 +602,7 @@ module.exports = function (webpackEnv) {
                         : undefined
                 )
             ),
+
             new DefinePlugin({
                 isOnline: process.env.NODE_ENV === 'production'
             }),
@@ -643,8 +642,8 @@ module.exports = function (webpackEnv) {
                 new MiniCssExtractPlugin({
                     // Options similar to the same options in webpackOptions.output
                     // both options are optional
-                    filename: 'static/css/[name].[contenthash:8].css',
-                    chunkFilename: 'static/css/[name].[contenthash:8].chunk.css'
+                    filename: 'static/css/sdk.css'
+                    // chunkFilename: 'static/css/[name].[contenthash:8].chunk.css'
                 }),
             // Generate an asset manifest file with the following content:
             // - "files" key: Mapping of all asset filenames to their corresponding
