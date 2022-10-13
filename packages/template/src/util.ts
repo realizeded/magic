01825b54bb5e342d2bfd764f75b5a0e5345a0484
lian <2235500875@qq.com>
2022-10-13 12:57:43 +0800

    feat: 更改预览地址

--- packages/template/src/util.ts
+++ packages/template/src/util.ts
@@ -1,4 +1,4 @@
 export const getUrl = (): string => {
     // return window.isOnline ? 'http://150.158.141.76:8800' : 'http://localhost:8800';
-    return 'http://150.158.141.76:8800';
+    return 'http://www.lmagic.work:8800';
 };
