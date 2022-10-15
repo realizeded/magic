df22545928debbdcdfba4fc9bf05681150b0c985
lian <2235500875@qq.com>
2022-10-15 16:08:43 +0800

    fix: 修复redux-devtool配置

--- packages/template/src/store/store.ts
+++ packages/template/src/store/store.ts
@@ -2,12 +2,9 @@ import { TRootState, TRootAction } from './type';
 import { createStore, combineReducers, compose } from 'redux';
 import { templateReducer } from './module/template';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true })
-    : compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const reducer = combineReducers({
     project: templateReducer
 });
-
 export const store = createStore<TRootState, TRootAction, unknown, unknown>(reducer, composeEnhancers);
