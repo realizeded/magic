4170640efe00905596892342fadfdfe9a2ffefe9
lian <2235500875@qq.com>
2022-11-19 20:46:01 +0800

    feat: 修复DefinePlugin不生效问题

--- packages/template/src/util.ts
+++ packages/template/src/util.ts
@@ -1,4 +1,6 @@
 export const getUrl = (): string => {
+    const isOnline = process.env.NODE_ENV !== 'development';
+
     // return window.isOnline ? 'http://150.158.141.76:8800' : 'http://localhost:8800';
-    return 'http://www.lmagic.work:8800';
+    return isOnline ? 'http://www.lmagic.work:8800' : 'http://localhost:8800';
 };
