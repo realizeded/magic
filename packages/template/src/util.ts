78f22b584f35bad9452a3c7ca24ebdfa9be35768
lian <2235500875@qq.com>
2022-11-27 17:25:52 +0800

    feat: 开启https

--- packages/template/src/util.ts
+++ packages/template/src/util.ts
@@ -2,7 +2,7 @@ export const getUrl = (): string => {
     const isOnline = process.env.NODE_ENV !== 'development';
 
     // return window.isOnline ? 'http://150.158.141.76:8800' : 'http://localhost:8800';
-    return isOnline ? 'http://www.lmagic.work:8800' : 'http://localhost:8800';
+    return isOnline ? 'https://client.lmagic.work' : 'http://localhost:8800';
 };
 
 export class Dep {
