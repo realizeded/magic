f3312c208f282bfaa0569eba76d284d48a5aa0d7
lian <2235500875@qq.com>
2023-02-16 00:06:24 +0800

    fix: 暂时修复静态资源路径问题

--- packages/template/src/util.ts
+++ packages/template/src/util.ts
@@ -8,7 +8,6 @@ export const getUrl = (): string => {
 export const getStaticUrl = (): string => {
     const isOnline = process.env.NODE_ENV !== 'development';
 
-    // return window.isOnline ? 'http://150.158.141.76:8800' : 'http://localhost:8800';
     return isOnline ? 'https://www.lmagic.work' : 'http://localhost:3000';
 };
 
